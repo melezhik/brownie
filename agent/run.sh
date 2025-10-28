6566051a5aa74367c9859508128ed95615887591
Alexey Melezhik <melezhik@gmail.com>
2025-10-28 09:11:47 +0300

    don't override names for agents

--- agent/run.sh
+++ agent/run.sh
@@ -4,6 +4,10 @@ docker stop -t 1 agent2
 
 sleep 3
 
-docker run -d --rm --name agent -e BRW_AGENT_NAME_PREFIX=agent1 -p 4002:4000 agent
+#docker run -d --rm --name agent -e BRW_AGENT_NAME_PREFIX=agent1 -p 4002:4000 agent
 
-docker run -d --rm --name agent2 -e BRW_AGENT_NAME_PREFIX=agent2 -p 4003:4000 agent
+#docker run -d --rm --name agent2 -e BRW_AGENT_NAME_PREFIX=agent2 -p 4003:4000 agent
+
+docker run -d --rm --name agent  -p 4002:4000 agent
+
+docker run -d --rm --name agent2 -p 4003:4000 agent
