5a742060d315fb92cf4bae9e0272008dddfbad1e
Alexey Melezhik <melezhik@gmail.com>
2025-10-29 09:11:25 +0300

    add debug info

--- agent/tasks/run-test/task.bash
+++ agent/tasks/run-test/task.bash
@@ -6,6 +6,13 @@ export ZEF_INSTALL_TO=$(config zef_install_to)
 
 export RAKULIB="inst#$ZEF_INSTALL_TO"
 
+
+echo "dump env vars"
+
+env | grep ZEF_INSTALL_TO
+
+env | grep RAKULIB
+
 if zef install $module 1>log.txt 2>&1; then
   echo "installation succeed"
   update_state success 1
@@ -13,3 +20,10 @@ if zef install $module 1>log.txt 2>&1; then
   echo "installation failed"
   update_state success 0
 fi
+
+echo "test log"
+
+echo "========"
+
+cat log.txt
+
