97a9613e851af20a55b2a66c613ba91c0d0435fe
Alexey Melezhik <melezhik@gmail.com>
2025-11-12 13:23:56 +0300

    debug

--- agent/run.sh
+++ agent/run.sh
@@ -4,6 +4,6 @@ docker stop -t 1 agent
 
 sleep 3
 
-docker run -d --rm --name agent -e BRW_AGENT_MAX_THREADS=7 -p 4002:4000 agent
+docker run -d --rm --name agent -e BRW_AGENT_MAX_THREADS=4 -p 4002:4000 agent
 
 #docker run -d --rm --name agent2 -p 4003:4000 agent
