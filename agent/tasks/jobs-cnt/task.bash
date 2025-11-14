27b3bee69e31906338a98f898d4b3925fb50099b
Alexey Melezhik <melezhik@gmail.com>
2025-11-14 16:41:41 +0300

    send local queue and running jobs stat to o10r

--- agent/tasks/jobs-cnt/task.bash
+++ agent/tasks/jobs-cnt/task.bash
@@ -1,13 +1,23 @@
 
 echo "jobs count ..."
 
-for i in $(find ~/.sparky/projects/ -name .triggers |grep agent.job); do ls -p $i| grep -v /  ; done | \
-wc -l > q.cnt
+for i in $(find ~/.sparky/projects/ -name .triggers |grep agent.job); do ls -p $i| grep -v /  ; done > q.data
 
+cat q.data | wc -l > q.cnt
 
+echo -n "local queue cnt: "
 cat q.cnt
 
-ps uax|grep sparky-run |grep "marker=agent.job"|grep -v grep| wc -l > r.cnt
+ps uax|grep sparky-run |grep "marker=agent.job"|grep -v grep > r.data
 
+cat r.data | wc -l > r.cnt
+
+echo -n "jobs running cnt: "
 
 cat r.cnt
+
+echo "local queue stat"
+cat q.data
+
+echo "jobs running stat"
+cat r.data
