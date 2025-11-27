72f55fff98cb479db4356cea5da5041efbbf996d
Alexey Melezhik <melezhik@gmail.com>
2025-11-27 14:46:09 +0300

    upgrade

--- agent/upgrade.sh
+++ agent/upgrade.sh
@@ -6,13 +6,27 @@ cd /opt/brownie/agent
 git pull
 bash sync.sh
 
-# TBD - terminate running project 
 
-echo "start agent main job"
-echo "===================="
+echo "terminate running agent"
+echo "======================="
+
+raku -e '
+if "{%*ENV<HOME>}/.sparky/work/agent/.states".IO ~~ :d {
+    for dir "{%*ENV<HOME>}/.sparky/work/agent/.states" -> $i { 
+        next if $i.basename ~~ /[".pid" || ".exit-code" || ".terminate"]  $$/; 
+        "{$i.path}.terminate".spurt();
+        say "send termination request as {$i.path}.terminate"; 
+    }
+}
+'
+
+echo "start new agent job"
+echo "==================="
 
 cd /root/.sparky/projects/agent/
+
 mkdir -p .triggers
+
 raku -e 'say %( :description<go_go_go> ).raku' > .triggers/go
 
 
