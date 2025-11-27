1d95348081a8f1d61208c0bd0ac0019d34837c66
Alexey Melezhik <melezhik@gmail.com>
2025-11-27 16:09:44 +0300

    rec

--- agent/sync.sh
+++ agent/sync.sh
@@ -2,4 +2,4 @@ set -e
 echo "sync agent"
 echo "==="
 mkdir -p  ~/.sparky/projects/agent/
-cp config.raku sparky.yaml sparrowfile tasks/ version  ~/.sparky/projects/agent/
+cp -r config.raku sparky.yaml sparrowfile tasks/ version  ~/.sparky/projects/agent/
