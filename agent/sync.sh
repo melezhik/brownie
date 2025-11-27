6390fbdbf2b165a2d024bdd2a44a6aac45b41367
Alexey Melezhik <melezhik@gmail.com>
2025-11-27 16:08:24 +0300

    copy that

--- agent/sync.sh
+++ agent/sync.sh
@@ -1,4 +1,4 @@
 set -e
 echo "sync agent"
 echo "==="
-cp sparky.yaml sparrowfile tasks/ version  ~/.sparky/projects/agent/
+cp config.raku sparky.yaml sparrowfile tasks/ version  ~/.sparky/projects/agent/
