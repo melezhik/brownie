08692de896d952ff5124bfc0f6770c2b0af00f51
Alexey Melezhik <melezhik@gmail.com>
2025-12-02 14:25:14 +0300

    Setup zef test environment and logging
    This script sets up the environment for running zef tests, logs the versions of raku and zef, and executes the zef test command for the specified module.

--- agent/tasks/zef-test/task.bash
+++ agent/tasks/zef-test/task.bash
@@ -1,1 +1,46 @@
+set -e
+
+echo "start test"
+
+module=$(config module)
+
+rakudo_version=$(config rakudo_version)
+
+old_path=$PATH
+
+export PATH=/tmp/whateverable/rakudo-moar/$rakudo_version/bin/:/tmp/whateverable/rakudo-moar/$rakudo_version/share/perl6/site/bin:$PATH
+
+echo "[raku]" > log.txt
+
+which raku >> log.txt
+
+raku --version >> log.txt
+
+echo "[zef]" >> log.txt
+
+which zef >> log.txt
+
+zef --version >> log.txt
+
+echo "===" >> log.txt
+
+echo "zef test $module" >> log.txt
+
+if ! test  "${zef_install_to}" = ""; then
+  #echo "zef_install_to is set to $zef_install_to, apply it"
+  export ZEF_INSTALL_TO=$zef_install_to
+  export RAKULIB="inst#$ZEF_INSTALL_TO"
+else
+  #echo "zef_install_to is empty, using standard zef path for install"
+  unset ZEF_INSTALL_TO
+fi
+
+zef test $module 1>>log.txt 2>&1 || :;
+
+echo "test log"
+
+echo "========"
+
+cat log.txt
+
 
